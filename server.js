f954babade201f51fd94bc93059790cc91576166
Ajay Srinivasa <ajaynsrinivas@gmail.com>
2017-01-25 16:05:49 +0530

    add todos and welcome page

--- server.js
+++ server.js
@@ -2,8 +2,46 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('/',function(req, res){
-    res.send('TODO Root');
+var todos = [
+    {
+        id:1,
+        description:'Have Lunch',
+        completed:false
+    },
+    {
+        id:2,
+        description:'Go to market',
+        completed:false
+    },
+    {
+        id:3,
+        description:'Go to park',
+        completed:true
+    }
+];
+
+app.use(express.static(__dirname+'/public'));
+
+// GET /todos
+app.get('/todos',function(req, res){
+   res.json(todos); 
+});
+
+// GET /todos/:id
+app.get('/todos/:id',function(req, res){
+    var todosId = req.params.id;
+    var matchingTodo;
+    todos.forEach(function(todo){
+      if(todo.id.toString() === todosId){
+          matchingTodo = todo;
+      }  
+    });
+    if(matchingTodo !== undefined){
+        res.json(matchingTodo);
+    }else{
+        console.log('Unable to find data for todo id:'+todosId);
+        res.status(404).send();
+    }
 });
 
 app.listen(PORT, function(){
