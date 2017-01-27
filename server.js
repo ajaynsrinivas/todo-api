8e0408fd06e319ece516a203c470aa2304f0c148
Ajay Srinivasa <ajaynsrinivas@gmail.com>
2017-01-27 17:26:42 +0530

    Added some validation

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
 var todoId = 1;
-
+var _ = require("underscore");
 app.use(bodyParser.json());
 var todos = [
     {
@@ -32,13 +32,13 @@ app.get('/todos',function(req, res){
 
 // GET /todos/:id
 app.get('/todos/:id',function(req, res){
-    var todosId = req.params.id;
-    var matchingTodo;
-    todos.forEach(function(todo){
-      if(todo.id.toString() === todosId){
-          matchingTodo = todo;
-      }  
-    });
+    var todosId = parseInt(req.params.id,10);
+    var matchingTodo = _.findWhere(todos, {id:todosId});
+//    todos.forEach(function(todo){
+//      if(todo.id.toString() === todosId){
+//          matchingTodo = todo;
+//      }  
+//    });
     if(matchingTodo !== undefined){
         res.json(matchingTodo);
     }else{
@@ -49,13 +49,16 @@ app.get('/todos/:id',function(req, res){
 
 // POST /todos
 app.post('/todos',function(req,res){
-    var body = req.body;
-    if(body.description !== undefined){
+    var body = _.pick(req.body,"description","completed");
+    if(_.isString(body.description)){
         todoId = todos.length + 1;
         body.id = todoId;
         todos.push(body);
+        res.json(body);
+    }else{
+        res.status(404).send();
     }
-    res.json(body);
+    
 });
 
 app.listen(PORT, function(){
